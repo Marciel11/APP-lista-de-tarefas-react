9a928cf65db95225f4c288333fa57cb6889ad07a
Marciel11 <marcielrodriguesd@gmail.com>
2023-08-21 18:05:53 -0300

    adicionado marcação lista, remover e pesquisar

---
+++ README.md
@@ -0,0 +1,4 @@
+---- Comando Executar o servidor Vite ----
+npm run dev
+
+http://127.0.0.1:5173/
--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import { useState } from 'react'
 import './App.css'
 import Todo from './components/Todo'
 import TodoForm from './components/TodoForm'
-
+import Search from './components/Search'
 
 function App() {
-  const [todos, setTodos] = useState([
 
+  const [todos, setTodos] = useState([
     {
       id: 1,
       text: "criar funcionalidade x no sistema",
@@ -25,8 +25,9 @@ function App() {
       category: "Estudos",
       isCompleted: false,
     }
-
   ])
+
+  const [search, setSearch] = useState("");
 
   const addTodo = (text, category) => {
     const newTodos = [
@@ -38,23 +39,36 @@ function App() {
         isCompleted: false,
       },
     ];
+    setTodos(newTodos);
+  };
 
-setTodos(newTodos);
+  const removeTodo = (id) => {
+    const newTodos = [...todos]
+    const filteredTodos = newTodos.filter((todo) =>
+      todo.id !== id ? todo : null
+    );
+    setTodos(filteredTodos);
+  };
 
-  }
+  const completeTodo = (id) => {
+    const newTodos = [...todos];
+    newTodos.map((todo) =>
+      todo.id === id ? (todo.isCompleted = !todo.isCompleted) : todo);
+    setTodos(newTodos);
+  };
 
   return (
     <div className="app">
       <h1>Lista de Tarefas</h1>
-
+      <Search search={search} setSearch={setSearch} />
       <div className="todo-list">
-        {todos.map((todo) => (
-
-          <Todo key={todo.id} todo={todo} />
-
+        {todos.filter((todo) => todo.text.toLowerCase().includes(search.toLowerCase())
+        ).map((todo) =>
+        (
+          <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo} />
         ))}
       </div>
-      <TodoForm addTodo = { addTodo } />
+      <TodoForm addTodo={addTodo} />
     </div>
   )
 }
